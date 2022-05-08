bcdb61f28fc53cfcc57817e7d32ec3277c07cec9
pmikovqa <105115774+pmikovqa@users.noreply.github.com>
2022-05-08 18:31:54 +0300

    Added new tests 08 05 2022

--- Summator/Program.cs
+++ Summator/Program.cs
@@ -6,7 +6,7 @@
         {
             int result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
 
-            if (result == 20)
+            if (result == 21)
             {
                 Console.WriteLine("Test Pass");
             }
@@ -15,6 +15,17 @@
             }
             double result2 = Summator.Average(new double[] { 6, 5 });   
             Console.WriteLine(result2);
-        }
+
+            double result3 = Summator.PowerOfNumbers(new double[] { 2, 2, });
+            Console.WriteLine(result3);
+
+            double biggest = Summator.BiggestNumber(new double[] { 2, 5, 8, 10, 6, 4 });
+            Console.WriteLine(biggest);
+            double smallest = Summator.SmallestNumber(new double[] { 2, 5, 8, 10, 6, 4 });
+            Console.WriteLine(smallest);
+
+            string newStatement = Summator.ReversedString(new string[] { "i", "n", "U", "t", "f", "o", "S" });
+            Console.WriteLine(newStatement);
+        }   
     }
 }
